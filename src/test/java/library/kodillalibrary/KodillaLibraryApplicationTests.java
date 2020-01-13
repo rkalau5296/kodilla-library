e8138cac327aa5911ddc6d4c8f19cb7e5a8a8513
rkalata <rkalata@kabbage.com>
2020-01-13 13:58:19 +0100

    first commit

---
+++ src/test/java/library/kodillalibrary/KodillaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package library.kodillalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
